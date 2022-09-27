9f240e61f018bcb8a6de404b2245506295bb5b9f
IrisIq <2637852337@qq.com>
2022-09-27 17:36:16 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter'; //失败拦截
+import { TransformInterceptor } from './core/interceptor/transform.interceptor'; //成功拦截器
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 设置全局路由前缀
+
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // 成功拦截器
+  // app.useGlobalFilters(new TransformInterceptor());
 
   // 启动服务器
   await app.listen(8717, () => {
